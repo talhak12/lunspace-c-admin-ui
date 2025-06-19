58400bd279758c01d493b565f44389bbc8302081
Talha Khan <talha4khan@hotmail.com>
2025-06-19 10:59:14 +0500

    add router

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,5 @@
+function Categories() {
+  return <h1>Hello from lun space</h1>;
+}
+
+export default Categories;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>Hello from mern space</h1>;
+}
+
+export default HomePage;
