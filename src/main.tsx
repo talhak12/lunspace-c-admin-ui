401c3cee3c63c30d603349329cef8701ca71a043
Talha Khan <talha4khan@hotmail.com>
2025-06-20 11:38:57 +0500

    add login form

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react';
+import { ConfigProvider } from 'antd';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider
+        theme={{ token: { colorPrimary: '#F65F42', colorLink: '#F65F42' } }}
+      >
+        <RouterProvider router={router} />
+      </ConfigProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,14 +1,104 @@
-import React from 'react';
+import { Layout, Card, Space, Form, Input, Checkbox, Button, Flex } from 'antd';
+import {
+  LockFilled,
+  UserOutlined,
+  LockOutlined,
+  ConsoleSqlOutlined,
+} from '@ant-design/icons';
+
+import Logo from '../../components/icons/Logo';
 
 const LoginPage = () => {
   return (
     <>
-      <h1>Sign in</h1>;
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Log in</button>
-      <label htmlFor="remember-me">Remember me</label>
-      <input type="checkbox" id="remember-me" />
+      <Layout
+        style={{ height: '100vh', display: 'grid', placeItems: 'center' }}
+      >
+        <Space direction="vertical" align="center" size="large">
+          <Layout.Content
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Logo />
+          </Layout.Content>
+          <Card
+            bordered={false}
+            style={{ width: 300 }}
+            title={
+              <Space
+                style={{
+                  width: '100%',
+                  fontSize: 16,
+                  justifyContent: 'center',
+                }}
+              >
+                <LockFilled />
+                Sign in
+              </Space>
+            }
+          >
+            <Form
+              initialValues={{ remember: true, username: 'test' }}
+              onFinish={(values) => {
+                console.log(values);
+              }}
+            >
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your user name',
+                  },
+                  {
+                    type: 'email',
+                    message: 'Email is not valid',
+                  },
+                ]}
+              >
+                <Input prefix={<UserOutlined />} placeholder="Username" />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password',
+                  },
+                ]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined />}
+                  placeholder="Password"
+                />
+              </Form.Item>
+
+              <Flex justify="space-between">
+                <Form.Item name="remember" valuePropName="checked">
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href="" id="login-form-forgot">
+                  Forgot Password
+                </a>
+              </Flex>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: '100%' }}
+                >
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Space>
+      </Layout>
     </>
   );
 };
