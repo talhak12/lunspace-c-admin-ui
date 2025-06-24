3c9150accc953fbc18da8ed1bc66b152da6ac238
Talha Khan <talha4khan@hotmail.com>
2025-06-24 19:38:51 +0500

    add hide menu items

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -5,50 +5,55 @@ import Layout, { Content, Footer, Header } from 'antd/es/layout/layout';
 
 import Sider from 'antd/es/layout/Sider';
 import { HomeOutlined, UserOutlined, BellFilled } from '@ant-design/icons';
-import {
-  Breadcrumb,
-  Menu,
-  theme,
-  Badge,
-  Flex,
-  Space,
-  Dropdown,
-  Avatar,
-} from 'antd';
+import { Menu, theme, Badge, Flex, Space, Dropdown, Avatar } from 'antd';
 import { useState } from 'react';
 import Logo from '../components/icons/Logo';
 import { useMutation } from '@tanstack/react-query';
 import { logout } from '../http/api';
 
-const items = [
-  {
-    key: '/',
-    icon: <HomeOutlined />,
-    label: <NavLink to="/">Home</NavLink>,
-  },
-  {
-    key: '/users',
-    icon: <UserOutlined />,
-    label: <NavLink to="/users">User</NavLink>,
-  },
-  {
-    key: '/restaurants',
-    icon: <UserOutlined />,
-    label: <NavLink to="/restaurants">Restaurants</NavLink>,
-  },
-  {
-    key: '/products',
-    icon: <UserOutlined />,
-    label: <NavLink to="/products">Products</NavLink>,
-  },
-  {
-    key: '/promos',
-    icon: <UserOutlined />,
-    label: <NavLink to="/promos">Promos</NavLink>,
-  },
-];
+const getMenuItems = (role: string) => {
+  const baseItems = [
+    {
+      key: '/',
+      icon: <HomeOutlined />,
+      label: <NavLink to="/">Home</NavLink>,
+    },
+
+    {
+      key: '/restaurants',
+      icon: <UserOutlined />,
+      label: <NavLink to="/restaurants">Restaurants</NavLink>,
+    },
+    {
+      key: '/products',
+      icon: <UserOutlined />,
+      label: <NavLink to="/products">Products</NavLink>,
+    },
+    {
+      key: '/promos',
+      icon: <UserOutlined />,
+      label: <NavLink to="/promos">Promos</NavLink>,
+    },
+  ];
+
+  if (role === 'admin') {
+    const menus = [...baseItems];
+
+    menus.splice(1, 0, {
+      key: '/users',
+      icon: <UserOutlined />,
+      label: <NavLink to="/users">User</NavLink>,
+    });
+    return menus;
+  }
+
+  return baseItems;
+};
 
 const Dashboard = () => {
+  const { user } = useAuthStore();
+  const items = getMenuItems(user?.role as string);
+
   const { logout: logoutFromStore } = useAuthStore();
 
   const { mutate: logoutMutate } = useMutation({
@@ -59,8 +64,6 @@ const Dashboard = () => {
       return;
     },
   });
-
-  const { user } = useAuthStore();
 
   const {
     token: { colorBgContainer },
