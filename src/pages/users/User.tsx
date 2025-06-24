21ba831db3b9b2f61bc2a5e64e9631a492aa623b
Talha Khan <talha4khan@hotmail.com>
2025-06-24 16:58:25 +0500

    add user dashboard

--- src/pages/users/User.tsx
+++ src/pages/users/User.tsx
@@ -1,16 +1,42 @@
 import { Breadcrumb } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { data, Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getUsers } from '../../http/api';
+import type { User } from '../../types';
 
-function Categories() {
+const Users = () => {
+  const logoutUser = async () => {
+    const { data } = await getUsers();
+    console.log('Users data:', data.user);
+    return data;
+  };
+
+  const { data } = useQuery({
+    queryKey: ['users'],
+    queryFn: logoutUser,
+  });
   return (
     <>
       <Breadcrumb
         separator={<RightOutlined />}
         items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Users' }]}
       />
+
+      {data && data.user && (
+        <div>
+          <h2>Users</h2>
+          <ul>
+            {data.user.map((user: User) => (
+              <li key={user.id}>
+                {user.fistName} {user.lastName} - {user.email}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
-}
+};
 
-export default Categories;
+export default Users;
