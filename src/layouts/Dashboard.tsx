00437c6ab96d97baec6e98e06f7b76a267121d85
Talha Khan <talha4khan@hotmail.com>
2025-06-28 21:39:11 +0500

    add intended url redirect

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Navigate, NavLink, Outlet } from 'react-router-dom';
+import { Navigate, NavLink, Outlet, useLocation } from 'react-router-dom';
 import { useAuthStore } from '../store';
 
 import Layout, { Content, Footer, Header } from 'antd/es/layout/layout';
@@ -51,6 +51,8 @@ const getMenuItems = (role: string) => {
 };
 
 const Dashboard = () => {
+  const location = useLocation();
+
   const { user } = useAuthStore();
   const items = getMenuItems(user?.role as string);
 
@@ -72,7 +74,12 @@ const Dashboard = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   if (user == null) {
-    return <Navigate to="/auth/login" replace={true} />;
+    return (
+      <Navigate
+        to={`/auth/login?returnTo=${location.pathname}`}
+        replace={true}
+      />
+    );
   }
   return (
     <div>
